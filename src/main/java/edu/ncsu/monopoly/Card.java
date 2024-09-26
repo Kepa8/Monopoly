fdc9477d29e96aed998d40f5b21312ec3caddf27
ehu <ehu@U032832>
2024-09-26 11:52:45 +0200

    yt

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -1,5 +1,5 @@
 package edu.ncsu.monopoly;
-//updated augost 24 with a comments.
+//updated augost 24 with a comments.df
 public abstract class Card {
 
     public static final int TYPE_CHANCE = 1;
